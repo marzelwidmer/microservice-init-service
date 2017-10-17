67e74ec115328e79aa79ea2efe1fb5ce0c3de919
Marcel Widmer <marzelwidmer@gmail.com>
2017-10-17 23:46:33 +0200

    fix `/` issue on 'persons' resource

--- src/main/java/ch/keepcalm/microservice/rogerrabbit/resource/person/PersonController.java
+++ src/main/java/ch/keepcalm/microservice/rogerrabbit/resource/person/PersonController.java
@@ -25,7 +25,7 @@ public class PersonController {
         this.personResourceAssembler = personResourceAssembler;
     }
 
-    @GetMapping(value = "/")
+    @GetMapping
     public ResponseEntity<Resources<PersonResource>> getPersons() {
         List<PersonResource> personResources = new ArrayList();
 
